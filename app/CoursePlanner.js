c4af700fbb8c067711d0516c57f545863557c503
Kevin <kph1958@rit.edu>
2014-11-16 19:44:36 -0500

    adding contenteditable back in for the titles

--- app/CoursePlanner.js
+++ app/CoursePlanner.js
@@ -107,6 +107,27 @@ app.directive('addcourse', function() {
   };
 });
 
+app.directive('contenteditable',function() {
+  return {
+    restrict: "A",
+    require: "ngModel",
+    link: function(scope, element, attrs, ngModel) {
+
+      function read() {
+        ngModel.$setViewValue(element.html());
+      }
+
+      ngModel.$render = function() {
+        element.html(ngModel.$viewValue || "");
+      };
+
+      element.bind("blur keyup change", function() {
+        scope.$apply(read);
+      });
+    }
+  };
+});
+
 app.directive('course', function() {
   return {
     restrict:'AE',
