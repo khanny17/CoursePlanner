36d60f48723bd9c3ce0cf7605422d775edeb3c75
Kevin <kph1958@rit.edu>
2014-12-09 15:49:39 -0500

    replacing userbutton directive with a simple ng-switch in the DOM

--- app/js/directives/auth.js
+++ app/js/directives/auth.js
@@ -12,44 +12,7 @@ angular.module("authDirectives",[])
           modal.removeClass('in');
         }
         prevUser = val;
-        
-      }); 
-    }
-  }
-})
 
-.directive('userbutton', function ($compile,$rootScope) {
-  return {
-    restrict: 'E',
-    link: function (scope, element, attr) {
-      var template1 = '<li><a href="#" id="login-modal-toggle"'+
-                      ' data-toggle="modal" data-target="#loginModal">Login</a></li>';
-                      
-      var newElem;
-      $rootScope.$watch('user', function (val) {
-        if (val==0) {
-          newElem = angular.element(template1);
-          $(element).replaceWith(newElem).show();
-          $compile($('#login-modal-toggle'))(scope);
-        } else {
-          var template2 = '<li class="dropdown">'+
-                      '<a href="#" class="dropdown-toggle" data-toggle="dropdown" '+
-                      'role="button" aria-expanded="false">'+val.local.username+'<span class="caret"></span></a>'+
-                      '<ul class="dropdown-menu" role="menu">'+
-                        '<li><a href="#">Action</a></li>'+
-                        '<li><a href="#">Another action</a></li>'+
-                        '<li><a href="#">Something else here</a></li>'+
-                        '<li class="divider"></li>'+
-                        '<li><a href="/logout">Logout</a></li></ul></li>'
-          
-
-          if(newElem==null) {
-            newElem = angular.element(template2);
-            element.replaceWith(newElem);
-          } else {
-            newElem.replaceWith(template2);
-          }
-        }
       });
     }
   }
