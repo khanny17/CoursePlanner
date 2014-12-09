98cad52a6904e52ae293ddb86e1b03bd0efc7837
Kevin <kph1958@rit.edu>
2014-12-09 15:10:59 -0500

    Commenting the routes

--- app/routes.js
+++ app/routes.js
@@ -1,13 +1,18 @@
+//Defines backend routes for the application
+
 var Plan = require('./models/plan');
 module.exports = function(app,passport) {
+  //The landing page
   app.get('/', function(req,res) {
     res.render('index.html');
   });
 
+  //The main application window
   app.get('/Planner', function(req,res) {
     res.render('main.html');
   });
 
+  //Route for saving plans to the database
   app.post('/Planner/save', function(req,res) {
     Plan.create({
       title : req.body.title,
@@ -27,6 +32,7 @@ module.exports = function(app,passport) {
 
   });
 
+  //Gets json of all plans
   app.get('/Planner/plans', function(req, res) {
     Plan.find(function(err, plans) {
       if(err)
@@ -36,23 +42,28 @@ module.exports = function(app,passport) {
     });
   });
 
+  //Returns the user if logged in or 0 if not
   app.get('/loggedin', function(req, res) {
     res.send(req.isAuthenticated() ? req.user : '0');
   });
 
+  //Attempts to authenticate user from request data
   app.post('/login', passport.authenticate('local-login'), function(req, res) {
     res.send(req.user);
   });
 
+  //Attempts to register a new user from request data
   app.post('/signup', passport.authenticate('local-signup'), function(req, res){
     res.send(req.user);
   });
 
+  //Logs the user out and returns them to the landing page
   app.get('/logout', function(req, res){
     req.logOut();
     res.render('index.html');
   });
 
+  //Used to test if user is authenticated - NOT WORKING and UNUSED
   var auth = function(req, res, next){
     if (!req.isAuthenticated())
       res.send(401);
